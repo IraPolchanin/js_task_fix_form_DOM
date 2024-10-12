9a3e849d9e7006c121f4b2399f0bdfd3da7e782b
Iryna Polchaninova <ira.polchanin@gmail.com>
2024-10-12 21:23:32 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write code here
+const inputsEl = [...document.querySelectorAll('.field-text')];
+
+inputsEl.forEach((el) => {
+  const labelEl = document.createElement('label');
+
+  labelEl.className = 'field-label';
+  labelEl.htmlFor = el.id;
+  labelEl.textContent = el.name;
+  el.parentNode.appendChild(labelEl);
+  el.placeholder = el.name;
+});
